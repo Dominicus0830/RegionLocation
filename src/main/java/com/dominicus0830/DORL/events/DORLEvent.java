79b8303d97650b45d8f786d327660e6dfe64962d
licker2689 <83544855+licker2689@users.noreply.github.com>
2023-03-18 20:12:45 +0900

    update

--- src/main/java/com/dominicus0830/DORL/events/DORLEvent.java
+++ src/main/java/com/dominicus0830/DORL/events/DORLEvent.java
@@ -1,15 +1,17 @@
 package com.dominicus0830.DORL.events;
 
 import com.dominicus0830.DORL.RegionLocation;
+import com.dominicus0830.DORL.func.DORLfunc;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class DORLEvent implements Listener {
-    private final RegionLocation plugin;
+    private RegionLocation plugin = RegionLocation.getInstance();
     private Location center;
     private int radius;
 
@@ -20,35 +22,16 @@ public class DORLEvent implements Listener {
     }
 
     @EventHandler
-    public void onBlockBreak(BlockBreakEvent event) {
-        Player player = event.getPlayer();
-        Location location = event.getBlock().getLocation();
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        Location location = event.getEntity().getLocation();
 
         // DORL 구역 내부가 아니면 이벤트를 취소한다.
-        if (!isInsideDORL(location)) {
-            player.sendMessage(ChatColor.RED + "구역 밖이에요!");
-            event.setCancelled(true);
+        if (!new DORLfunc().isInsideDORL(location)) {
+            event.setDeathMessage(null);
         }
     }
 
     // 지정된 위치가 DORL 구역 내부인지 확인하는 메소드
-    private boolean isInsideDORL(Location location) {
-        // 현재 위치의 x, y, z 좌표
-        int x = location.getBlockX();
-        int y = location.getBlockY();
-        int z = location.getBlockZ();
 
-        // DORL 구역의 중심 좌표
-        Location center = plugin.getCenter();
-        // DORL 구역의 반지름
-        int radius = plugin.getRadius();
-
-        // DORL 구역 내에 있는지 확인
-        if (x >= center.getBlockX() - radius && x <= center.getBlockX() + radius
-                && y >= center.getBlockY() - radius && y <= center.getBlockY() + radius
-                && z >= center.getBlockZ() - radius && z <= center.getBlockZ() + radius) {
-            return true;
-        }
-        return false;
-    }
 }
